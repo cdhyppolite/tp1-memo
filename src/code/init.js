950aeaa0d3adc623faaa5637bc9107ed0330592e
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-03-25 13:18:27 -0400

    fonction connexion

--- src/code/init.js
+++ src/code/init.js
@@ -1,25 +1,17 @@
 import firebaseConfig from './config';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 
-/***************************************
- * Firebase
- **************************************/
 // Initialiser Firebase
+export const instanceFirebase = initializeApp(firebaseConfig);
 
-
-/***************************************
- * Firebase Authentication
- **************************************/
-// Initialiser Firebase Authentication
+// Initialiser Firebase Authentification
+export const authFirebase = getAuth(instanceFirebase);
 
 // Initialiser l'authentification fédérée Google
+export const authGoogle = new GoogleAuthProvider();
 
-
-/***************************************
- * Firestore
- ***************************************/
 // Initialiser Firestore
-
+export const bdFirestore = getFirestore();
--- src/composants/Accueil.jsx
+++ src/composants/Accueil.jsx
@@ -2,11 +2,20 @@ import './Accueil.scss';
 import logoGoogle from '../images/google-logo.png';
 
 export default function Accueil() {
+  
+  // Déclenche le processus d'authentification avec Google Auth Provider
+   function connexion() {
+    alert('À compléter !');
+    signInWithPopup(authFirebase, authGoogle).then(
+      userGoogle => setUtil(userGoogle.user)
+    );
+  }
+
   return (
     <div className="Accueil">
       <h3 className="logo">Memo</h3>
       <div className="conteneur-boutons-connexion">
-        <span className="btn-google" onClick={() => alert('À compléter !')}>
+        <span className="btn-google" onClick={connexion}>
           <img className="btn-image" src={logoGoogle} alt="Logo Google"/>
           <span className="btn-texte">Continuer avec Google</span>
         </span>
