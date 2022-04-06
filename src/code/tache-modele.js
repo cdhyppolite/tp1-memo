396271847efafee26637bd7a15fb50a2e119c7dd
cdhyppolite <92059993+cdhyppolite@users.noreply.github.com>
2022-04-06 19:51:36 -0400

    Date v2

--- src/code/tache-modele.js
+++ src/code/tache-modele.js
@@ -22,10 +22,17 @@ export async function creerTache(idUtilisateur, tache) {
 
 //  Convertir la date
 export function convertirDate(dateEnTimestamp) {
-    const date = new Date(dateEnTimestamp * 1000).toLocaleString();
-    const jour = date.substring(0, 2);
+    const date = new Date(dateEnTimestamp * 1000);
+    const annee = date.getFullYear();
     const listeDesMois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
-    const mois = listeDesMois[parseInt(date.substring(3, 5)) - 1];
-    const annee = date.substring(6, 10);
-    return `(${jour} ${mois} ${annee} à ${date.substring(12, 20)})`;
+    const mois = listeDesMois[date.getMonth()];
+    const jour = date.getDate();
+    return `(${jour} ${mois} ${annee} à ${heureDeuxChiffre( date.getHours(),date.getMinutes(), date.getSeconds() )})`;
+}
+
+function heureDeuxChiffre(heures, minutes, secondes) {
+    let h = (heures < 10 ? '0' : '') + heures;
+    let m = (minutes < 10 ? '0' : '') + minutes;
+    let s = (secondes < 10 ? '0' : '') + secondes;
+    return `${h}:${m}:${s}`
 }
